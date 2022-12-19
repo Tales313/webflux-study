abb44d3d69cd229fabeab60146c0f94a65e35792
Tales313 <t4les313@gmail.com>
2022-12-19 16:29:51 -0300

    reactive endpoints

---
+++ webflux-demo/src/main/java/com/example/webfluxdemo/controller/ReactiveMathController.java
@@ -0,0 +1,30 @@
+package com.example.webfluxdemo.controller;
+
+import com.example.webfluxdemo.dto.Response;
+import com.example.webfluxdemo.service.ReactiveMathService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+
+@RestController
+@RequestMapping("/reactive-math")
+public class ReactiveMathController {
+
+    @Autowired
+    private ReactiveMathService reactiveMathService;
+
+    @GetMapping("square/{input}")
+    public Mono<Response> findSquare(@PathVariable int input) {
+        return this.reactiveMathService.findSquare(input);
+    }
+
+    @GetMapping("table/{input}")
+    public Flux<Response> multiplicationTable(@PathVariable int input) {
+        return this.reactiveMathService.multiplicationTable(input);
+    }
+}
