51cfbeab8f574557be564eba87498d507a929fde
Tales313 <t4les313@gmail.com>
2022-12-19 16:51:36 -0300

    alterando media type no endpoint e vendo resultado do flux no browser

--- webflux-demo/src/main/java/com/example/webfluxdemo/controller/ReactiveMathController.java
+++ webflux-demo/src/main/java/com/example/webfluxdemo/controller/ReactiveMathController.java
@@ -3,6 +3,7 @@ package com.example.webfluxdemo.controller;
 import com.example.webfluxdemo.dto.Response;
 import com.example.webfluxdemo.service.ReactiveMathService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,6 +26,11 @@ public class ReactiveMathController {
 
     @GetMapping("table/{input}")
     public Flux<Response> multiplicationTable(@PathVariable int input) {
+        return this.reactiveMathService.multiplicationTable(input);
+    }
+
+    @GetMapping(value = "table/{input}/stream", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<Response> multiplicationTableStream(@PathVariable int input) {
         return this.reactiveMathService.multiplicationTable(input);
     }
 }
